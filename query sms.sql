9d3a5f3f90cc2d54cadab9e70b9cd71c6a545102
Shelvi Garg <garg.shelvi@gmail.com>
2021-02-24 23:10:14 +0530

    Add files via upload

---
+++ query sms.sql
@@ -0,0 +1,13 @@
+use school;
+
+create table teacher(n varchar(255),post varchar(255),salary varchar(255),address varchar(255),phone varchar(255),acc varchar(255));
+create table student(n varchar(255),class_studying varchar(255),roll varchar(255),address varchar(255),phone varchar(255));
+create table student(n varchar(255),class_studying varchar(255),roll varchar(255),address varchar(255),phone varchar(255));
+create table cattendance (d varchar(255),class_studying varchar(255),names_absent_student varchar(255));
+create table tattendance(d varchar(255),names_absent_teacher varchar(255));
+create table fees(n varchar(255),class_studying varchar(255),roll varchar(255),m varchar(255),y varchar(255),fees varchar(255),d varchar(255));
+create table salary(n varchar(255),m varchar(255),y varchar(255),stat varchar(255));
+
+
+
+
